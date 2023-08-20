1abcfbeb8607ede07a430f951bc524a24c837397
Diego Moreira <diegomoreira@MacBook-Pro-de-Diego.local>
2023-08-20 11:18:59 -0300

    Subindo projeto base

---
+++ opiniaoplay/opiniaoplayApp.swift
@@ -0,0 +1,17 @@
+//
+//  opiniaoplayApp.swift
+//  opiniaoplay
+//
+//  Created by Diego Moreira on 20/08/23.
+//
+
+import SwiftUI
+
+@main
+struct opiniaoplayApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ opiniaoplayUITests/opiniaoplayUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  opiniaoplayUITestsLaunchTests.swift
+//  opiniaoplayUITests
+//
+//  Created by Diego Moreira on 20/08/23.
+//
+
+import XCTest
+
+final class opiniaoplayUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
